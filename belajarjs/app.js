aaf66422ff6d8056685bdcbdce8cbb54092af5d9
monster ngamuok <140871214+ucihariskoding@users.noreply.github.com>
2023-12-08 22:39:29 +0700

    latihan logic and or

--- belajarjs/app.js
+++ belajarjs/app.js
@@ -19,14 +19,23 @@ if (nilai>80){
     console.log('dongok lu ..');
 }
 
-const passW = prompt('masukkan paswot');
+// const passW = prompt('masukkan paswot');
 
-if (passW.length >=6){
-    if (passW.indexOf(' ')===-1){
-        console.log('paswot PALID');
-    } else {
-        console.log('paswot invalid -> mengandung spasi');
-    }
+// if (passW.length >=6){
+//     if (passW.indexOf(' ')===-1){
+//         console.log('paswot PALID');
+//     } else {
+//         console.log('paswot invalid -> mengandung spasi');
+//     }
+// } else {
+//     console.log('paswot kureng dari 6 KARAKTER');
+// }
+
+let umor = 20;
+let gender = 'loki';
+
+if (gender === 'laki' && umor>=18){
+    console.log ('sudah gedeee');
 } else {
-    console.log('paswot kureng dari 6 KARAKTER');
+    console.log('bocil kamu dekkk');
 }
